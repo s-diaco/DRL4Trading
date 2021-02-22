97ea6ac3ef58f7ebab85744eb95721000337309a
Diaco <dyaco8464@yahoo.com>
2021-02-22 11:17:19 +0330

    works on multiple files in current dir

--- convert_csv_data.py
+++ convert_csv_data.py
@@ -2,34 +2,37 @@
 import pandas as pd
 import numpy as np
 import pandas as pd
+import glob
+import os
   
-#%% csv file name 
-import_filename = "بترانس"
-exp_filename = "tse"
-#filename = "dow_30_2009_2020"
-#import_filename = "test"
-fileext = "csv"
+# %% csv files
+path = os.getcwd()
+all_files = glob.glob(path + "/*.csv")
+exp_filename = "combined_tse.csv"
 
-# %% read the file
-df = pd.read_csv(import_filename+"."+fileext) 
-df.head()
-      
-# %% change and remove some columns
-df = df.drop(['count', 'value', 'adjClose'], axis = 1)
-df = df.rename(columns=({'date':'datadate', 
-                        'open':'prcod',
-                        'high':'prchd',
-                        'low':'prcld',
-                        'volume':'cshtrd',
-                        'close':'prccd'}))
-df['datadate'] = df['datadate'].map(lambda x: x.replace('-',''))
-df["ajexdi"] = 1.0
-df["tic"] = import_filename
-cols = ['datadate', 'tic', 'prccd', 'ajexdi', 'prcod', 'prchd', 'prcld', 'cshtrd'] 
-df = df[cols]
-df = df[df['datadate'] >= '20090101']
-df = df.reset_index(drop=True)
-df.head()
-# %% save to the file
-df.to_csv(exp_filename+'.'+fileext, index=1, encoding='utf-8')
+li = []
+
+for filename in all_files:
+    df = pd.read_csv(filename, index_col=None, header=0)
+    df.head()
+    df = df.drop(['count', 'value', 'adjClose'], axis = 1)
+    df = df.rename(columns=({'date':'datadate', 
+                            'open':'prcod',
+                            'high':'prchd',
+                            'low':'prcld',
+                            'volume':'cshtrd',
+                            'close':'prccd'}))
+    df['datadate'] = df['datadate'].map(lambda x: x.replace('-',''))
+    df["ajexdi"] = 1.0
+    df["tic"] = os.path.split(filename)[1]
+    cols = ['datadate', 'tic', 'prccd', 'ajexdi', 'prcod', 'prchd', 'prcld', 'cshtrd'] 
+    df = df[cols]
+    df = df[df['datadate'] >= '20090101']
+    li.append(df)
+
+#%%
+frame = pd.concat(li, axis=0, ignore_index=True)
+frame.to_csv('all_csvs', index=1, encoding='utf-8')
+frame.head()
+
 # %%
