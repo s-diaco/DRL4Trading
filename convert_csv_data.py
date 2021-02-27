bc28d9eeed23a1e544482e6f60c2790001d243d8
Diaco <dyaco8464@yahoo.com>
2021-02-27 10:25:29 +0000

    first model created

--- convert_csv_data.py
+++ convert_csv_data.py
@@ -1,9 +1,9 @@
 # %% common library
-import pandas as pd
-import numpy as np
-import pandas as pd
 import glob
 import os
+
+import numpy as np
+import pandas as pd
 
 # %% csv files
 in_dir = 'import_csv'
