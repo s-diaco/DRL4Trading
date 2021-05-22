a0716604e8366786d3fff59771d0eeac0688d3b7
Diaco <dyaco8464@yahoo.com>
2021-05-22 08:14:21 +0000

    chkpt

--- model/models_test.py
+++ model/models_test.py
@@ -6,3 +6,5 @@ class TestTradeDRLAgent:
         root_dir = 'root_dir'
         str_1, str_2, str_3 = models.TradeDRLAgent()._get_model_dirs(root_dir=root_dir)
         assert isinstance(str_1, str), "dir is not a string"
+        assert isinstance(str_2, str), "dir is not a string"
+        assert isinstance(str_3, str), "dir is not a string"
