3bb363dc087de8d3c5dc1a820380fd279b867dd5
Diaco <dyaco8464@yahoo.com>
2021-06-13 04:42:22 +0000

    switched to single process

--- preprocess_tse_data.py
+++ preprocess_tse_data.py
@@ -5,14 +5,14 @@ from numpy.core.multiarray import where
 from numpy.core.numeric import zeros_like
 from config import config
 import pandas as pd
-from get_tse_data.tse_data import tse_data
+from get_tse_data.tse_data import TSEData
 from typing import Tuple
 import logging
 
 # matplotlib.use('Agg')
 # get_ipython().run_line_magic('matplotlib', 'inline')
 
-def preprocess_data(tic_list = config.TSE_TICKER_5) -> Tuple[pd.DataFrame, pd.DataFrame]:
+def preprocess_data(tic_list = config.TSE_TICKER_5, add_custom_columns=True) -> Tuple[pd.DataFrame, pd.DataFrame]:
 
     #if not os.path.exists("./" + config.DATA_SAVE_DIR):
     #    os.makedirs("./" + config.DATA_SAVE_DIR)
@@ -28,9 +28,10 @@ def preprocess_data(tic_list = config.TSE_TICKER_5) -> Tuple[pd.DataFrame, pd.Da
     # from config.py end_date is a string
     logging.info(f'End date: {config.END_DATE}')
     logging.info(f'Tickers: {tic_list}')
-    raw_df = tse_data(start_date=config.START_DATE,
+    tse_data = TSEData(start_date=config.START_DATE,
                 end_date=config.END_DATE,
-                ticker_list=tic_list).fetch_data()
+                ticker_list=tic_list)
+    raw_df = tse_data.fetch_data()
 
     # 4.Preprocess Data
     feat_eng = FeatureEngineer(
@@ -41,47 +42,48 @@ def preprocess_data(tic_list = config.TSE_TICKER_5) -> Tuple[pd.DataFrame, pd.Da
 
     processed = feat_eng.preprocess_data(raw_df)
 
-    # processed['log_volume'] = np.log(processed.volume*processed.close)
-    processed['change'] = np.divide(
-        ((processed.close-processed.open)/processed.close),
-        ((processed.index_close-processed.index_yesterday)/processed.index_close),
-        out=np.ones_like(processed.index_close),
-        where=(processed.index_close-processed.index_yesterday)!=0)
-    processed['daily_variance'] = np.divide(
-        processed.high-processed.low,
-        processed.close,
-        out=zeros_like(processed.close),
-        where=processed.close!=0)
-    processed['volume_ma_ratio'] = np.divide(
-        processed.volume_5_sma,
-        processed.volume_30_sma,
-        out=np.ones_like(processed.close),
-        where=processed.volume_30_sma!=0)
-    processed['count_ma_ratio'] = np.divide(
-        processed.count_5_sma,
-        processed.count_30_sma,
-        out=np.ones_like(processed.close),
-        where=processed.count_30_sma!=0)
-    processed['ma_ratio'] = np.divide(
-        processed.close_5_sma,
-        processed.close_30_sma,
-        out=np.ones_like(processed.close),
-        where=processed.close_30_sma!=0)
-    processed['indv_buy_sell_ratio'] = np.divide(
-        processed.individual_buy_count,
-        processed.individual_sell_count,
-        out=np.zeros_like(processed.close),
-        where=processed.individual_sell_count!=0)
-    processed['corp_buy_sell_ratio'] = np.divide(
-        processed.corporate_buy_count,
-        processed.corporate_sell_count,
-        out=np.zeros_like(processed.close),
-        where=processed.corporate_sell_count!=0)
-    processed['ind_corp_buy_ratio'] = np.divide(
-        processed.individual_buy_vol,
-        processed.corporate_buy_vol,
-        out=np.zeros_like(processed.close),
-        where=processed.corporate_buy_vol!=0)
+    if add_custom_columns:
+        # processed['log_volume'] = np.log(processed.volume*processed.close)
+        processed['change'] = np.divide(
+            ((processed.close-processed.open)/processed.close),
+            ((processed.index_close-processed.index_yesterday)/processed.index_close),
+            out=np.ones_like(processed.index_close),
+            where=(processed.index_close-processed.index_yesterday)!=0)
+        processed['daily_variance'] = np.divide(
+            processed.high-processed.low,
+            processed.close,
+            out=zeros_like(processed.close),
+            where=processed.close!=0)
+        processed['volume_ma_ratio'] = np.divide(
+            processed.volume_5_sma,
+            processed.volume_30_sma,
+            out=np.ones_like(processed.close),
+            where=processed.volume_30_sma!=0)
+        processed['count_ma_ratio'] = np.divide(
+            processed.count_5_sma,
+            processed.count_30_sma,
+            out=np.ones_like(processed.close),
+            where=processed.count_30_sma!=0)
+        processed['ma_ratio'] = np.divide(
+            processed.close_5_sma,
+            processed.close_30_sma,
+            out=np.ones_like(processed.close),
+            where=processed.close_30_sma!=0)
+        processed['indv_buy_sell_ratio'] = np.divide(
+            processed.individual_buy_count,
+            processed.individual_sell_count,
+            out=np.zeros_like(processed.close),
+            where=processed.individual_sell_count!=0)
+        processed['corp_buy_sell_ratio'] = np.divide(
+            processed.corporate_buy_count,
+            processed.corporate_sell_count,
+            out=np.zeros_like(processed.close),
+            where=processed.corporate_sell_count!=0)
+        processed['ind_corp_buy_ratio'] = np.divide(
+            processed.individual_buy_vol,
+            processed.corporate_buy_vol,
+            out=np.zeros_like(processed.close),
+            where=processed.corporate_buy_vol!=0)
     logging.info(f'Preprocessed data (tail): \n {processed.tail()}')
     
     # 5.Design Environment
